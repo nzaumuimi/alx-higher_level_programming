f00d7b70fb8b045b736330b283f5aba956b20fb6
nzaumuimi <nzaumuimi@gmail.com>
2024-01-15 12:20:12 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/5-main.py
+++ 0x0C-python-almost_a_circle/5-main.py
@@ -8,4 +8,14 @@ if __name__ == "__main__":
     print(r1)
 
     r2 = Rectangle(5, 5, 1)
+    print(r2)#!/usr/bin/python3
+""" 5-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(4, 6, 2, 1, 12)
+    print(r1)
+
+    r2 = Rectangle(5, 5, 1)
     print(r2)
