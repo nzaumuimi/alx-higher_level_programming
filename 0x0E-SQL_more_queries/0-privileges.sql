8aebc71e49a7321418dbb363a08cd203ddf0ebe2
nzaumuimi <nzaumuimi@gmail.com>
2024-02-16 03:25:44 -0800

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that lists all privileges of MySQL users
+-- The Users are user_0d_1 and user_0d_2
+-- Server(in localhost)
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
